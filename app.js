6ece3c95423c2633609dcf0d1dd0b1d758f18703
Martyna Wloka <wlok4.mar@gmail.com>
2025-03-26 10:15:27 +0000

    Navbar

---
+++ app.js
@@ -0,0 +1,11 @@
+//NAVBAR
+const menuLinks = document.querySelectorAll(".menu-link");
+
+menuLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		menuLinks.forEach((link) => {
+			link.classList.remove("is-active");
+		});
+		link.classList.add("is-active");
+	});
+});
